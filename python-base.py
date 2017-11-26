821ea8293832b9b46d2b052039d150cc8e5dc10a
makai <kai.fantasy@gmail.com>
2017-11-26 10:11:05 +0800

    基本语法

--- python-base.py
+++ python-base.py
@@ -7,7 +7,7 @@ import sys
 
 print("python's keyword", keyword.kwlist)
 
-## input("\n\n 按 回车 键退出。 ")
+# input("\n\n 按 回车 键退出。 ")
 
 print("命令行参数为： ")
 
@@ -28,7 +28,6 @@ print('冻结集合：frozenset')
 print('布尔类型：True, False')
 print('None')
 
-print('在Python 3里，只有一种整数类型 int，表示为长整型，没有 python2 中的 Long。')
 
 print('----- 愉快的分割线 ---------')
 
@@ -38,7 +37,7 @@ octal = 0o177, 0x9ff, 0X9FF, 0b101010  # 八进制
 change = hex(10), oct(10), bin(10)
 nums = float('inf'), float('-inf'), float('nan')
 name = 'kai.fantasy'  # 字符串
-a, b, c = 1, 2, 'kai' ## 多变量赋值还可以这样玩
+a, b, c, d = 1, 100.234, True, 'kai'  # 多变量赋值还可以这样玩
 
 print(counter)
 print(miles)
@@ -47,3 +46,23 @@ print(name)
 print('将十进制数转化为十六进制、八进制、二进制表示的“字符串', change)
 print('无穷大, 无穷小, 非数', nums)
 print(a, b, c)
+
+print('---- 在Python 3里，只有一种整数类型 int，表示为长整型，没有 python2 中的 Long。----')
+
+print('## type() 函数可以用来查询变量所指向的对象类型')
+
+print(type(a), type(b), type(c), type(d))
+
+print('也可以用 isinstance 来判断')
+
+print(isinstance(a, int)
+
+
+print('isinstance 和 type d 区别')
+
+print('type 不会认为子类是一种父类类型')
+print('isinstance 会认为子类是一种父类类型')
+
+
+print('在 Python2 中是没有布尔型的，它用数字 0 表示 False，用 1 表示 True。到 Python3 中，把 True 和 False 定义成关键字了，但它们的值还是 1 和 0，它们可以和数字相加。')
+		
