8f34c655b00cb799f7614eeb248b4aecc10621c4
makai <kai.fantasy@gmail.com>
2017-11-27 22:37:22 +0800

    python 运算

--- python-operation.py
+++ python-operation.py
@@ -41,3 +41,56 @@ a = 10
 b = 5
 c = a // b
 print('a // b c = ', c)
+
+
+print('Python比较运算符')
+
+print()
+print('==	等于 - 比较对象是否相等')
+print('!=	不等于 - 比较两个对象是否不相等')
+print('>	大于 - 返回x是否大于y')
+print('<	小于 - 返回x是否小于y。所有比较运算符返回1表示真，返回0表示假。这分别与特殊的变量True和False等价。注意，这些变量名的大写。	')
+print('>=	大于等于 - 返回x是否大于等于y。')
+print('<=	小于等于 - 返回x是否小于等于y。')
+
+
+print('')
+print('Python赋值运算符')
+print('=	简单的赋值运算符	c = a + b 将 a + b 的运算结果赋值为 c')
+print('+=	加法赋值运算符	c += a 等效于 c = c + a')
+print('-=	减法赋值运算符	c -= a 等效于 c = c - a')
+print('*=	乘法赋值运算符	c *= a 等效于 c = c * a')
+print('/=	除法赋值运算符	c /= a 等效于 c = c / a')
+print('%=	取模赋值运算符	c %= a 等效于 c = c % a')
+print('**=	幂赋值运算符	c **= a 等效于 c = c ** a')
+print('//=	取整除赋值运算符	c //= a 等效于 c = c // a')
+
+a = 21
+b = 10
+c = 0
+
+print('a = ', a)
+print('b = ', b)
+print('c = ', c)
+
+c = a + b
+print(' c = a + b : ', c)
+
+c += a
+print(' c += a : ', c)
+
+c *= a
+print(' c += a : ', c)
+
+c /= a
+print(' c /= a : ', c)
+
+c = 2
+c %= a
+print(' c = 2 c %= a : ', c)
+
+c **= a
+print(' c **= a : ', c)
+
+c //= a
+print(' c //= a : ', c)
