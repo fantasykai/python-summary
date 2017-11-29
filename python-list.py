15f02cdba2a0ad0762f25a38daeaea45973c10c8
makai <kai.fantasy@gmail.com>
2017-11-29 16:25:41 +0800

    基础语法list

---
+++ python-list.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3.6
+
+print('Python有6个序列的内置类型，但最常见的是列表和元组。')
+print('序列都可以进行的操作包括索引，切片，加，乘，检查成员。')
+print('Python已经内置确定序列的长度以及确定最大和最小的元素的方法。')
+print('列表的数据项不需要具有相同的类型')
+
+list1 = ['Google', 'Bing', 'Baidu', 2017, True, 1000.20]
+list2 = [1, 2, 3, 4, 5, 6, 7]
+list3 = ['a', 'b', 'c', 'd', 'e']
+
+print('list1 = ', list1)
+print('list2 = ', list2)
+print('list3 = ', list3)
