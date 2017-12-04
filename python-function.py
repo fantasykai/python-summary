d97c8b7ae9b729ed1b8246c6f40dc2ea219d793a
makai <kai.fantasy@gmail.com>
2017-12-04 20:57:48 +0800

    函数

--- python-function.py
+++ python-function.py
@@ -107,6 +107,7 @@ print("默认参数")
 
 print("调用函数时，如果没有传递参数，则会使用默认参数")
 
+print("默认参数不在最后，会报错")
 
 def printInfo(name, age=30):
     print("name : ", name)
@@ -140,3 +141,122 @@ def printInfo(arg1, *vartuple):
 
 print(10)
 print(20, 30, 40, 50)
+
+print("匿名函数")
+print("python 使用 lambda 来创建匿名函数")
+print("所谓匿名，意即不再使用 def 语句这样标准的形式定义一个函数")
+
+print("lambda 只是一个表达式，函数体比 def 简单很多")
+print("lambda的主体是一个表达式，而不是一个代码块。仅仅能在lambda表达式中封装有限的逻辑进去")
+print("lambda 函数拥有自己的命名空间，且不能访问自己参数列表之外或全局命名空间里的参数。")
+print("虽然lambda函数看起来只能写一行，却不等同于C或C++的内联函数，后者的目的是调用小函数时不占用栈内存从而增加运行效率。")
+
+print("语法")
+
+print("lambda 函数的语法只包含一个语句")
+
+print(" lambda [arg1 [,arg2,.....argn]]:expression ")
+print("lambda 匿名函数也是可以使用"关键字参数"进行参数传递, 同样地，lambda 匿名函数也可以设定默认值")
+
+
+def sum(arg1, arg2): return arg1 + arg2
+## sum = lambda arg1, arg2: arg1 + arg2;
+
+
+print("相加后的值 ： ", sum(10, 20))
+
+print("return语句 ")
+print(
+    "return [表达式] 语句用于退出函数，选择性地向调用方返回一个表达式。不带参数值的return语句返回None。之前的例子都没有示范如何返回数值")
+
+
+def sum(arg1, arg2):
+    total = arg1 + arg2
+    print("total : ", total)
+    return total
+
+
+total1 = sum(30, 20)
+print("total1 : ", total1)
+
+
+print("变量作用域: ")
+print("Python 中，程序的变量并不是在哪个位置都可以访问的，访问权限决定于这个变量是在哪里赋值的")
+print("变量的作用域决定了在哪一部分程序可以访问哪个特定的变量名称。Python的作用域一共有4种，分别是")
+print("L （Local） 局部作用域")
+print("E （Enclosing） 闭包函数外的函数中")
+print("G （Global） 全局作用域")
+print("B （Built-in） 内建作用域")
+print()
+print("以 L –> E –> G –>B 的规则查找，即：在局部找不到，便会去局部外的局部找（例如闭包），再找不到就会去全局找，再者去内建中找。")
+
+
+x = int(2.9)  # 内建作用域
+
+g_count = 0  # 全局作用域
+
+
+def outer():
+    o_count = 1  # 闭包函数外的函数中
+
+    def inner():
+        o_count = 2  # 局部作用域
+
+
+print("Python 中只有模块（module），类（class）以及函数（def、lambda）才会引入新的作用域，其它的代码块（如 if/elif/else/、try/except、for/while等）是不会引入新的作用域的，也就是说这这些语句内定义的变量，外部也可以访问")
+
+print("全局变量和局部变量")
+print("定义在函数内部的变量拥有一个局部作用域，定义在函数外的拥有全局作用域。")
+print("局部变量只能在其被声明的函数内部访问，而全局变量可以在整个程序范围内访问。调用函数时，所有在函数内声明的变量名称都将被加入到作用域中")
+print("")
+total = 0  # 这是一个全局变量
+# 可写函数说明
+
+
+def sum(arg1, arg2):
+    # 返回2个参数的和."
+    total = arg1 + arg2  # total在这里是局部变量.
+    print("函数内是局部变量 : ", total)
+    return total
+
+
+# 调用sum函数
+sum(10, 20)
+print("函数外是全局变量 : ", total)
+
+
+print()
+
+print("global 和 nonlocal关键字")
+print("当内部作用域想修改外部作用域的变量时，就要用到global和nonlocal关键字了")
+
+num = 1
+
+
+def fun1():
+    global num  # 需要使用 global 关键字声明
+    print(num)
+    num = 123
+    print(num)
+
+
+fun1()
+
+
+print()
+
+print("如果要修改嵌套作用域（enclosing 作用域，外层非全局作用域）中的变量则需要 nonlocal 关键字了")
+
+
+def outer():
+    num = 10
+
+    def inner():
+        nonlocal num
+        num = 100
+        print(num)
+    inner()
+    print(num)
+
+
+outer()
