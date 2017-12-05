70ecce6f7f53357288621ad27aa6ac100405d025
makai <kai.fantasy@gmail.com>
2017-12-05 11:53:39 +0800

    数据结构

--- python-dataStructure.py
+++ python-dataStructure.py
@@ -44,3 +44,21 @@ queue.append("360")
 queue.popleft()
 
 print("queue : ", queue)
+
+print()
+
+print("列表推导式")
+
+print("列表推导式提供了从序列创建列表的简单途径。通常应用程序将一些操作应用于某个序列的每个元素，用其获得的结果作为生成新列表的元素，或者根据确定的判定条件创建子序列。每个列表推导式都在 for 之后跟一个表达式，然后有零到多个 for 或 if 子句。返回结果是一个根据表达从其后的 for 和 if 上下文环境中生成出来的列表。如果希望表达式推导出一个元组，就必须使用括号。")
+
+
+vec = [2, 4, 6]
+
+vec1 = [3 * x for x in vec]
+
+print('vec1: ', vec1)
+
+vec2 = [[x, x**2] for x in vec]
+
+
+print("vec2: ", vec2)
