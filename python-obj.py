8782853a90bf0763c993656379ca89bfce836504
makai <kai.fantasy@gmail.com>
2017-12-09 14:40:39 +0800

    python 面向对象

--- python-obj.py
+++ python-obj.py
@@ -62,3 +62,20 @@ class Complex(object):
 
 x = Complex(3.0, -4.5)
 print(x.r, x. i)
+
+print()
+print("self代表类的实例，而非类")
+
+print("类的方法与普通的函数只有一个特别的区别——它们必须有一个额外的第一个参数名称, 按照惯例它的名称是 self。")
+
+print()
+
+
+class Test:
+    def prt(self):
+        print(self)
+        print(self.__class__)
+
+
+t = Test()
+t.prt()
