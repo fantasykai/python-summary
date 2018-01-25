31e7e8e8124eae3c82dd7bd82aa38bed55324bd2
makai <kai.fantasy@gmail.com>
2018-01-25 16:04:45 +0800

    python 面向对象

--- python-obj.py
+++ python-obj.py
@@ -220,3 +220,78 @@ class sample(speaker, student):
 
 test = sample("Tim", 25, 80, 4, "Python")
 test.speak()
+
+
+print()
+print("方法重写")
+print("如果你的父类方法的功能不能满足你的需求，你可以在子类重写你父类的方法，实例如下：")
+
+
+class Parent:        # 定义父类
+    def myMethod(self):
+        print('调用父类方法')
+
+
+class Child(Parent):  # 定义子类
+    def myMethod(self):
+        print('调用子类方法')
+
+
+c = Child()          # 子类实例
+c.myMethod()         # 子类调用重写方法
+
+print()
+print("类属性与方法")
+print("类的私有属性")
+print("__private_attrs：两个下划线开头，声明该属性为私有，不能在类地外部被使用或直接访问。在类内部的方法中使用时 self.__private_attrs。")
+print("类的方法")
+print("在类地内部，使用 def 关键字来定义一个方法，与一般函数定义不同，类方法必须包含参数 self，且为第一个参数，self 代表的是类的实例。")
+print("self 的名字并不是规定死的，也可以使用 this，但是最好还是按照约定是用 self。")
+print("类的私有方法")
+print("__private_method：两个下划线开头，声明该方法为私有方法，只能在类的内部调用 ，不能在类地外部调用。")
+print("self.__private_methods。")
+print("实例")
+print("类的私有属性实例如下：")
+
+
+class JustCounter:
+    __secretCount = 0  # 私有变量
+    publicCount = 0  # 公有变量
+
+    def count(self):
+        self.__secretCount += 1
+        self.publicCount += 1
+        print(self.__secretCount)
+
+
+counter = JustCounter()
+counter.count()
+counter.count()
+print(counter.publicCount)
+# print(counter.__secretCount)## 报错，实例不能访问私有变量
+#
+print()
+print("类的私有方法实例如下：")
+
+
+class Site:
+    def __init__(self, name, url):
+        self.name = name       # public
+        self.__url = url   # private
+
+    def who(self):
+        print('name  : ', self.name)
+        print('url : ', self.__url)
+
+    def __foo(self):          # 私有方法
+        print('这是私有方法')
+
+    def foo(self):            # 公共方法
+        print('这是公共方法')
+        self.__foo()
+
+
+x = Site('个人博客', 'fantasykai.cc')
+x.who()
+x.foo()
+# x.__foo() 报错
