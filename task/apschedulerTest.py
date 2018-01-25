e6975c5fa9001a0f9c49bbb4b41c20fec66ff995
makai <kai.fantasy@gmail.com>
2018-01-25 16:24:24 +0800

    python 定时任务简单demo

--- task/apschedulerTest.py
+++ task/apschedulerTest.py
@@ -13,6 +13,10 @@ def aps_test(x):
 
 
 scheduler = BlockingScheduler()
-# scheduler.add_job(func=aps_test, trigger='cron', second='*/5')
+scheduler.add_job(func=aps_test, trigger='cron', second='*/5')
 scheduler.add_job(func=aps_test, args=('带个参数带执行任务，呵呵哒',), trigger='cron', second='*/5')
+scheduler.add_job(func=aps_test, args=('定时任务',), trigger='cron', second='*/5')
+scheduler.add_job(func=aps_test, args=('一次性任务',),
+                  next_run_time=datetime.datetime.now() + datetime.timedelta(seconds=12))
+scheduler.add_job(func=aps_test, args=('循环任务',), trigger='interval', seconds=3)
 scheduler.start()
