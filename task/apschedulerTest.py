5f7b740020cd0ed6cc78d5e594317027bfd2c296
makai <kai.fantasy@gmail.com>
2018-01-25 16:20:41 +0800

    python 定时任务简单demo

--- task/apschedulerTest.py
+++ task/apschedulerTest.py
@@ -8,6 +8,11 @@ def aps_test():
     print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '定时执行，呵呵哒')
 
 
+def aps_test(x):
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), x)
+
+
 scheduler = BlockingScheduler()
-scheduler.add_job(func=aps_test, trigger='cron', second='*/5')
+# scheduler.add_job(func=aps_test, trigger='cron', second='*/5')
+scheduler.add_job(func=aps_test, args=('带个参数带执行任务，呵呵哒',), trigger='cron', second='*/5')
 scheduler.start()
